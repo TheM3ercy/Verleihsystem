e2ef3472f62069129aa15fe61d88102d649583c6
SchusselCodes <83704967+SchusselCodes@users.noreply.github.com>
2022-04-22 12:30:43 +0200

    Mvvm Service implemented

---
+++ Verleisystem/Services/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Verleihsystem.Services
+{
+    internal class RelayCommand<T> : ICommand
+    {
+        private readonly Action<T> _execute;
+        private readonly Predicate<T> _canExecute;
+
+        public RelayCommand(Action<T?> execute) : this(execute, null)
+        { }
+
+        public RelayCommand(Action<T?> execute, Predicate<T?> canExecute)
+        { 
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object? parameter) 
+            => _canExecute == null || (parameter == null ? _canExecute(default) : _canExecute((T)parameter));
+
+        public void Execute(object? parameter)
+        {
+            if (parameter == null) _execute(default);
+            else _execute((T)parameter);
+        }
+    }
+}
---
+++ Verleisystem/ViewModels/BorrowProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verleihsystem.Services;
+
+namespace Verleihsystem.ViewModels
+{
+    internal class BorrowProductViewModel : ObservableObject
+    {
+    }
+}
