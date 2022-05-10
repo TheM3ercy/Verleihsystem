8baf92f3f42e2ffcd89806efbf7dfe4b4cec7afe
TheM3ercy <t.k.furjan@gmail.com>
2022-05-10 19:45:40 +0200

    Name Change of User Control

--- Verleisystem/UserControls/CustomerUserControl.xaml.cs
+++ Verleisystem/UserControls/CustomerUserControl.xaml.cs
@@ -17,35 +17,35 @@ using System.Windows.Shapes;
 namespace Verleihsystem.UserControls
 {
     /// <summary>
-    /// Interaction logic for Customer.xaml
+    /// Interaction logic for CustomerUserControl.xaml
     /// </summary>
-    public partial class Customer : UserControl
+    public partial class CustomerUserControl : UserControl
     {
-        public Customer()
+        public CustomerUserControl()
         {
             InitializeComponent();
         }
 
-        [Category("Data"), Description("Name of Customer")]
+        [Category("Data"), Description("Name of CustomerUserControl")]
         public string CustomerName
         {
             get { return (string)customername.Content; }
             set { customername.Content = value; }
         }
-        [Category("Data"), Description("ID of Customer")]
+        [Category("Data"), Description("ID of CustomerUserControl")]
         public string CustomerId
         {
             get { return (string)customerid.Content; }
             set { customerid.Content = value; }
         }
 
-        [Category("Data"), Description("Telephonenumber of Customer")]
+        [Category("Data"), Description("Telephonenumber of CustomerUserControl")]
         public string CustomerTel
         {
             get { return (string)customerTel.Content; }
             set { customerTel.Content = value; }
         }
-        [Category("Data"), Description("Email of Customer")]
+        [Category("Data"), Description("Email of CustomerUserControl")]
         public string ProductName
         {
             get { return (string)customerEmail.Content; }
